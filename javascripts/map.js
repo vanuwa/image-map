ae269ba310bdec4e47936a529f8cd339d7a998e7
Va Nuwa <vanuwa@gmail.com>
2011-12-02 13:09:36 +0200

    folder structer

---
+++ javascripts/map.js
@@ -0,0 +1,35 @@
+(function() {
+ 
+  SETTINGS = {
+    version: '0.0.1',
+    env: 'dev'
+  };
+  
+
+  var Point = (function() {
+    
+    var x,y,name,description;
+    var size = 4;
+    var color = 'blue';
+
+    function Point() {
+    }
+
+    return Map;
+
+  })();
+
+  var Map = (function() {
+
+    function Map() {
+    }
+
+    return Map;
+  
+  })(); 
+
+  this.Map = Map;
+
+  this.Point = Point;
+
+ }).call(this);
